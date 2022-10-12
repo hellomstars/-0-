583b7c0d45ef478d4384b7251ef104ac2bfc731d
unknown <hellomstars@naver.com>
2022-10-12 23:21:15 +0900

    사칙연산, 조건문, 배열

---
+++ 서현/1012.py
@@ -0,0 +1,15 @@
+#정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+
+def solution(num1, num2):
+    answer = int(num1/num2*1000)
+    return answer
+
+#정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+def solution(num1, num2):
+    if num1==num2 :
+        answer = 1
+    else :
+        answer = -1
+    return answer
+
+#첫 번째 분수의 분자와 분모를 뜻하는 denum1, num1, 두 번째 분수의 분자와 분모를 뜻하는 denum2, num2가 매개변수로 주어집니다. 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
