e35554042c2712f3b3e8bd8f760636f5ed05cfbd
unknown <hellomstars@naver.com>
2022-10-13 21:15:33 +0900

    사칙연산, 배열, 수학

---
+++ SEOHYUN/1013.py
@@ -0,0 +1,26 @@
+#정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+#중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
+def solution(array):
+    array.sort()
+    center = len(array) // 2 
+    if len(array)% 2 == 1:
+        answer = array[center]
+    return answer
+
+#최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
+
+#정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+def solution(n):
+    answer = []
+    for i in range (n+1):
+        if i % 2 == 1:
+            answer.append(i)
+    return answer
+
+    # 다른 사람들은?
+    #def solution(n):
+    #   return [i for i in range(1, n+1, 2)]
