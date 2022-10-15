2bfb1aa11115b88ae984e66586fc960c13770ffe
JJIMINSHIN <scalla1031@gmail.com>
2022-10-15 17:35:58 +0900

    두 수의 나눗셈

--- JIMIN/1015_1.js
+++ JIMIN/1015_1.js
@@ -1,8 +1,8 @@
 const solution = (a,b) => {
-    let answer = (a / b)*1000;
+    let answer = Math.floor((a / b)*1000);
     return answer;
 }
 
 console.time()
-console.log(solution(3,2))
+console.log(solution(7,3))
 console.timeEnd()
