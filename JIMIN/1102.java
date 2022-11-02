f9213b61b734a4f341a9ec0ab2e13846ddc080d8
unknown <scalla1031@gmail.com>
2022-11-02 15:33:44 +0900

    나이출력 -java version

---
+++ JIMIN/1102.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int age) {
+       
+        int nowYear = 2022;
+        int answer = nowYear - age+1;
+        return answer;
+    }
+
+    public void main(String[] args) {
+        solution(40);
+    }
+}
