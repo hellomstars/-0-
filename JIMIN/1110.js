a86bfd63e5077410e83f77d403ed5db3ebfd32ff
JJIMINSHIN <scalla1031@gmail.com>
2022-11-14 16:07:43 +0900

    양꼬치 -재풀이

--- JIMIN/1110.js
+++ JIMIN/1110.js
@@ -1,12 +1,6 @@
-const solution = (n, k) => {
-    let answer = 0;
-    let s = 0;
-    if (n >= 10) {
-        k -= n / 10;
-    }
-    answer = n*12000+k*2000;
-
-    return answer;
+function solution(n, k) {
+    return n * 12000 + (k - Math.floor(n/10)) * 2000
 }
 
-console.log(solution(64, 6));
+console.log('10 이상',solution(64, 6));
+console.log('10 미만',solution(3,3));
