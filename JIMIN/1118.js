52e9db130c5b6f551853cc6c70e018d7776e571a
JJIMINSHIN <scalla1031@gmail.com>
2022-11-18 16:13:04 +0900

    배열 두 배 만들기

---
+++ JIMIN/1118.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    let answer = []
+    numbers.forEach((item) => {
+        item *= 2
+        answer.push(item)
+    });
+    return answer
+}
+
+let numbers = [2, 4, 6, 8, 10]
+console.log(solution(numbers));
+
+
+///map으로 다시 풀어보기
