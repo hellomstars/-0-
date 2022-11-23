c953fe9beb726fd56812c9a1ac3f1106aea13937
unknown <scalla1031@gmail.com>
2022-11-24 08:54:26 +0900

    배열의 평균값

--- JIMIN/1124.js
+++ JIMIN/1124.js
@@ -1,5 +1,5 @@
 //배열의 평균값
-function solution(numbers) {
+function solution1(numbers) {
     
     let answer = 0;
     let size = numbers.length;
@@ -10,5 +10,11 @@ function solution(numbers) {
     return answer;
 }
 
+const solution2 = (numbers)=>{
+    return numbers.reduce((a,b) => a+b ,0 )/ numbers.length;
+}
+
+
 let numbers= [89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
-console.log(solution(numbers));
+console.log(solution1(numbers));
+console.log(solution2(numbers));
