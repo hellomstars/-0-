d9175126ab8f29af03c97d0ac2032574c52f976e
unknown <scalla1031@gmail.com>
2022-12-05 12:50:37 +0900

    k의 개수

--- JIMIN/1202.js
+++ JIMIN/1202.js
@@ -1,23 +1,13 @@
 // k의 개수
 
 function solution(i, j, k) {
-    let answer = 0;
-    let result=0;
-    for (i; i <= j; i++) {
-        result=i.toString().split('').filter(v => v == k.toString()); 
-        if (result.length > 0) {
-            answer++
-            //console.log(result)
-            for(let results of result){
-                console.log(results)
-            }
-           
-        }
-
-        //console.log(i.toString().split(',').filter(v => v == k.toString()))
+    let answer = 0;  
+    for(;i<=j;i++){
+        answer += i.toString().split('').map(Number).filter(v => v==k).length;
     }
 
     return answer
 }
 
 console.log(solution(1, 13, 1))
+//console.log(solution(10, 50, 5))
