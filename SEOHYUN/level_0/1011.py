66e87b46a89f4e261c3bebdadb210368873532bb
unknown <hellomstars@naver.com>
2022-12-07 22:04:13 +0900

    사칙연산

---
+++ SEOHYUN/level_0/1011.py
@@ -0,0 +1,19 @@
+#정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = num1*num2
+    return answer
+
+#정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = num1+num2
+    return answer
+
+#정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = num1-num2
+    return answer
+
+#정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+def solution(num1, num2):
+    answer = num1//num2
+    return answer
