877b601ddbdc9ce356a93089cd4967cea2ea11df
JJIMINSHIN <scalla1031@gmail.com>
2022-12-14 16:50:18 +0900

    잘라서 배열로 저장하기

--- JIMIN/1213-1214.js
+++ JIMIN/1213-1214.js
@@ -1,10 +1,24 @@
 // 잘라서 배열로 저장하기
 
 function solution(my_str, n) {
+    let str = my_str.split('');
+    let answer = [];
+
+    while (str.length > 0) {
+        answer.push(str.splice(0, n).join(''));
+    }
+
+    return answer;
+}
+console.log(solution("abcdef123abcdef123", 6));
+
+
+/*
+function solution(my_str, n) {
     let answer =[];
     answer.push(my_str.substring(0,n));
     answer.push(my_str.substring(n,n+n));
     answer.push(my_str.substring(n+n, ));
     return answer;
 }
-console.log(solution("abcdef123", 3));
+*/
