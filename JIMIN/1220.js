568ed6d00171efda46260590f2417108f73b962e
JJIMINSHIN <scalla1031@gmail.com>
2022-12-20 16:32:18 +0900

    옷가게 할인 받기

---
+++ JIMIN/1220.js
@@ -0,0 +1,9 @@
+//옷가게 할인 받기
+
+function solution(price) {
+    return price >= 500000 ? Math.floor(price *0.8) : price >= 300000 ? Math.floor(price *0.9) : price >= 100000 ? Math.floor(price * 0.95) : price;
+}
+
+console.log(solution(10))
+
+
