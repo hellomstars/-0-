a9019ed9ff5d18c7de0225783ea588c243748273
unknown <scalla1031@gmail.com>
2023-01-02 17:15:09 +0900

    중앙값 구하기

--- JIMIN/2023/0102.js
+++ JIMIN/2023/0102.js
@@ -1,6 +1,6 @@
 function solution(array) {
-    var answer = array.map(v => v+v);
-    return answer;
+    var answer = array.sort((a,b) => a-b);
+    return  Math.floor((array.reduce((acr, cur) => acr+cur))/array.length);
 }
 
 console.log(solution([1, 2, 7, 10, 11]));
