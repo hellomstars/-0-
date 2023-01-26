b3336e53f3da2f9ddc0031d63a23551d6d8a282f
CHAN <pigyechan@gmail.com>
2023-01-27 01:13:54 +0900

    commit

--- YECHAN/012623.py
+++ YECHAN/012623.py
@@ -9,3 +9,23 @@ def solution(str1, str2):
 def solution(n):
     return 1 if (n**0.5).is_integer() else 2
 
+#개미 군단
+def solution(hp):
+    ant = [5,3,1]
+    cnt = 0
+    a = 0
+    
+    for i in ant:
+        a = hp // i
+        cnt += a
+        hp = hp % i 
+        
+    return cnt
+
+#모음제거
+def solution(my_string):
+    mo = ['a', 'e', 'i', 'o', 'u']
+    for i in mo:
+        my_string = my_string.replace(i, '')
+        
+    return my_string 
