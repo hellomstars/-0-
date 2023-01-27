1bbdf69e83f0b333c93c8bf99ab3cab625d4caf7
unknown <calla31@jcone.co.kr>
2023-01-27 09:25:42 +0900

    로그인 성공?

---
+++ JIMIN/2023/0127.js
@@ -0,0 +1,8 @@
+function solution(id_pw, db) {
+    var answer = '';
+    return answer;
+}
+
+let id_pw = ["meosseugi", "1234"];
+let db = [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]];
+console.log(solution(id_pw, db));
