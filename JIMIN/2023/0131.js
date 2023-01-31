c13089f8c9487fc63f69322a264643fe339342b5
JJIMINSHIN <scalla1031@gmail.com>
2023-01-31 20:52:37 +0900

    로그인 성공(1)

--- JIMIN/2023/0131.js
+++ JIMIN/2023/0131.js
@@ -1,6 +1,11 @@
+// 로그인 성공?
+
 function solution(id_pw, db) {
-    var answer = '';
+    db.forEach((item) =>{
+        item[0] !== id_pw[0] ? answer = 'fail' : item[1] == id_pw[1] ? answer = 'login'  : answer = 'wrong pw' ;
+    });
     return answer;
+
 }
 
 let id_pw = ["meosseugi", "1234"];
