1c147def27a428687da06d7104628700f41ba7a0
JJIMINSHIN <86641936+JJIMINSHIN@users.noreply.github.com>
2023-02-01 14:50:23 +0900

    UPDATE : 로그인 성공? (최종)

--- JIMIN/2023/0131-0201.js
+++ JIMIN/2023/0131-0201.js
@@ -9,6 +9,7 @@ function solution(id_pw, db) {
     });
     return answer;
 }
+
 let id_pw = ["meosseugi", "1234"];
 let db = [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]];
 console.log(solution(id_pw, db));
