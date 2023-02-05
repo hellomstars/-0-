5153faba06ccffc7356bad2c778fb4ab5ed4076a
JJIMINSHIN <scalla1031@gmail.com>
2023-02-05 16:11:24 +0900

    UPDATE: 숫자 찾기 - pretty

--- JIMIN/2023/0205.js
+++ JIMIN/2023/0205.js
@@ -3,4 +3,11 @@
 function solution(num, k) {
    return num.toString().split('').indexOf(k+'') == -1 ? -1 : [...num+''].findIndex((v) => v == k.toString())+1;
 }
+
+// pretty
+const solution2 = (num,k)=>{ 
+    return num.toString().split('').findIndex((v) => v ==k )+1 || -1
+}
+
 console.log(solution(29183,0));
+console.log(solution2(29183,1));
